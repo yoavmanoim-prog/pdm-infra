4767e3dfa0cd3a5ddfab1af54021f4e6b7260dd4
Yoav <yoav992@gmail.com>
2026-05-31 19:49:01 +0300

    change autoscaler settings for node group to allow more nodes if needed

--- modules/pdm-env/main.tf
+++ modules/pdm-env/main.tf
@@ -50,8 +50,8 @@ module "eks" {
     default = {
       instance_types = ["t3.small"]
       min_size       = 1
-      max_size       = 2
-      desired_size   = 1
+      max_size       = 3
+      desired_size   = 2
     }
   }
 }
